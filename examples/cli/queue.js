4fef0a7b0391de3e5fc56e787eee5461e4e09078
Jake Luer <jake.luer@gmail.com>
2012-06-29 10:29:53 -0400

    clean up cli example for screenshots

--- examples/cli/queue.js
+++ examples/cli/queue.js
@@ -1,4 +1,4 @@
-var kinetik = require('..')
+var kinetik = require('../..')
   , queue = kinetik();
 
 var action = function (job, next) {
@@ -6,37 +6,37 @@ var action = function (job, next) {
 }
 
 queue
-  .define('task:1')
+  .define('messaging.one')
   .desc('This is the first task')
   .tag('messaging')
   .action(action);
 
 queue
-  .define('task:message')
+  .define('messaging.two')
   .desc('This is another messaging task')
   .tag('messaging')
   .action(action);
 
 queue
-  .define('task:urgent')
+  .define('urgent.one')
   .desc('this is an urgent task')
   .tag('urgent')
   .action(action);
 
 queue
-  .define('task:2')
+  .define('urgent.two')
   .desc('this is another urgent task')
   .tag('urgent')
   .action(action);
 
 queue
-  .define('task:ping')
+  .define('urgent.three')
   .desc('this is another last urgent')
   .tag('urgent')
   .action(action);
 
-[ 'task:1', 'task:message', 'task:urgent'
-, 'task:2', 'task:ping' ].forEach(function (task) {
+[ 'messaging.one', 'messaging.two', 'urgent.one'
+, 'urgent.two', 'urgent.three' ].forEach(function (task) {
   var c = Math.floor(Math.random() * 10) || 1;
   for (var i = 0; i < c; i++) {
     queue.create(task, {
